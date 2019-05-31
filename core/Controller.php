727718791a6f7e5758894f0c6862ad7b73b2a87a
angryshan <1264689637@qq,com>
2019-05-31 16:41:09 +0800

    '11'

--- core/Controller.php
+++ core/Controller.php
@@ -8,15 +8,9 @@ use core\Response;
 
 class Controller{
 
-    public $request;
-
-    public $response;
-
     public $pool = [];
 
     public function __construct(){
-        $this->request = Request::$request;
-        $this->response = Response::$response;
         $this->init();
     }
 
@@ -25,13 +19,13 @@ class Controller{
     public function json($data = ['' => ''], $code = 200, $msg = '成功'){
         $return = json_encode(['code' => $code, 'msg' => $msg, 'data' => $data]);
         echo date('Y-m-d H:i:s') . PHP_EOL . $return . PHP_EOL . PHP_EOL;
-        $this->response->status($code);
-        $this->response->end($return);
+        Response::$response->status($code);
+        Response::$response->end($return);
     }
 
     public function send($return){
         echo date('Y-m-d H:i:s') . PHP_EOL . $return . PHP_EOL . PHP_EOL;
-        $this->response->end($return);
+        Response::$response->end($return);
     }
 
     public function getPoolObj($name){
@@ -51,12 +45,12 @@ class Controller{
     public function input($index = ''){
         # 获取所有
         if (empty($index)){
-            if ($this->request->get && $this->request->post) {
-                $param = array_merge($this->request->get, $this->request->post);
-            }else if(!$this->request->get && $this->request->post){
-                $param = $this->request->post;
-            }else if($this->request->get && !$this->request->post){
-                $param = $this->request->get;
+            if (Request::$request->get && Request::$request->post) {
+                $param = array_merge(Request::$request->get, Request::$request->post);
+            }else if(!Request::$request->get && Request::$request->post){
+                $param = Request::$request->post;
+            }else if(Request::$request->get && !Request::$request->post){
+                $param = Request::$request->get;
             }else{
                 $param = null;
             }
@@ -65,16 +59,16 @@ class Controller{
             if (in_array(strtolower($index[0]), ['get', 'post'])){
                 if ($index[1] == ''){
                     $index = $index[0];
-                    $param = $this->request->$index;
+                    $param = Request::$request->$index;
                 }else{
-                    $param = $this->request;
+                    $param = Request::$request;
                     foreach ($index as $item) {
                         $param = $param[$item];
                     }
                 }
             }else{
-                $param_get = isset($this->request->get[$index[0]])? $this->request->get[$index[0]] : '';
-                $param_post = isset($this->request->post[$index[0]])? $this->request->post[$index[0]] : '';
+                $param_get = isset(Request::$request->get[$index[0]])? Request::$request->get[$index[0]] : '';
+                $param_post = isset(Request::$request->post[$index[0]])? Request::$request->post[$index[0]] : '';
                 if (!$param_post && !$param_get){
                     $param = null;
                 }else{
